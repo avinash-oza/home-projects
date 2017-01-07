36846136f9de5be064263306ad6e485b959db8f4
Avi Oza <avioza@gmail.com>
2017-01-07 13:59:46 -0500

    Add logging portion and use config for settings. Fix start script

--- pi/get_case_temperature.sh
+++ pi/get_case_temperature.sh
@@ -1,5 +1,5 @@
-cd /home/pi//home-projects/pi/
+#! /bin/bash
+cd /usr/local/nagios/libexec/case_temperature
 source /etc/bash_completion.d/virtualenvwrapper
 source /home/asterisk/.virtualenvs/telegram-bot/bin/activate
 python get_temperature.py
-exit $?
--- pi/get_temperature.py
+++ pi/get_temperature.py
@@ -1,10 +1,25 @@
+import sys
 import subprocess
 import re
+import ConfigParser
+import mysql.connector
+
+config = ConfigParser.ConfigParser()
+config.read('bot.config')
 
 CRITICAL_TEMP = 71.0
 
 class TemperatureReciever(object):
     """Retrieves temperatures from the sensors"""
+    def __init__(self):
+        db_host_name = config.get('DATABASE', 'host')
+        db_user_name = config.get('DATABASE', 'user')
+        db_password = config.get('DATABASE', 'password')
+        database_name = config.get('DATABASE', 'database')
+
+        # construct the connection object to use later
+        self.conn =  mysql.connector.connect(user=db_user_name,password=db_password,host=db_host_name, database=database_name)
+
     def get_case_temperature(self):
         command_to_run="""cat /sys/bus/w1/devices/28-000004f236cf/w1_slave"""
         output = subprocess.check_output([command_to_run], shell=True)
@@ -15,12 +30,22 @@ class TemperatureReciever(object):
 
         # Convert temperature to F
         f_degrees = 1.8*float(temperature)/1000 + 32
+        # Write the temperature to DB
+        self.store_temperature("CASE_TEMPERATURE", f_degrees)
         print "Case Temperature: {0}F".format(f_degrees)
         if f_degrees > CRITICAL_TEMP:
-            exit(2)
+            return 2
         else:
-            exit(0)
+            return 0
+
+    def store_temperature(self, sensor_name, sensor_value):
+        # Write out the value to the db
+        query = """INSERT INTO `temperatures`(sensor_name, sensor_value) VALUES('{0}', {1})""".format(sensor_name, sensor_value)
+        cursor = self.conn.cursor(buffered=True)
+        cursor.execute(query)
+        # Commit the insert
+        self.conn.commit()
 
 if __name__ == '__main__':
     o = TemperatureReciever()
-    o.get_case_temperature();
+    sys.exit(o.get_case_temperature())
