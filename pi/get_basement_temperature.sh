5764c023d7a55c6a44d2714a46978d3d55b2fa26
Avi Oza <avioza@gmail.com>
2017-01-09 16:56:30 -0500

    Add script for basement temperature logging

---
+++ pi/get_basement_temperature.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+cd /usr/lib/nagios/plugins/case_temperature
+source /etc/bash_completion.d/virtualenvwrapper
+source /home/asterisk/.virtualenvs/telegram-bot/bin/activate
+python get_temperature.py
