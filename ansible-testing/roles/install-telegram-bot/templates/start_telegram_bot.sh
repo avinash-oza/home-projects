c45f94e1ee90f0905eb6a1f3b4afd13301527f91
Avi <avioza@gmail.com>
2017-04-15 00:15:46 -0400

    Add changes for debian 7 and temp disable systemmd script

--- ansible-testing/roles/install-telegram-bot/templates/start_telegram_bot.sh
+++ ansible-testing/roles/install-telegram-bot/templates/start_telegram_bot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source {{ telegram_bot_virtual_env_location }}/bin/activate
 cd {{ telegram_checkout_location }}
-python start_telegram_bot.py
+nohup python start_telegram_bot.py >> telegram-bot.log 2>&1&
