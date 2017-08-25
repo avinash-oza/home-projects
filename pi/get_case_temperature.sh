e5dc9a19e3a5e6164f39fbadbc057653287b9235
Avi Oza <avioza@gmail.com>
2017-08-25 17:14:05 -0400

    Fix paths

--- pi/get_case_temperature.sh
+++ pi/get_case_temperature.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd /usr/lib/nagios/plugins/case_temperature
-source /home/telegram-bot/telegram-virtual-env/bin/activate
+source /home/telegram_bot/telegram-virtual-env/bin/activate
 python get_temperature.py --case
