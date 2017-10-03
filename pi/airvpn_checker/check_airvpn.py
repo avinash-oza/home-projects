73e6e77af55d8edbf7cf2db0a38a93ebb5c15d45
Avi Oza <avioza@gmail.com>
2017-10-02 22:15:56 -0400

    Remove print statement to stop email

--- pi/airvpn_checker/check_airvpn.py
+++ pi/airvpn_checker/check_airvpn.py
@@ -20,9 +20,8 @@ except:
     exit_code = "2"
 else:
     if(len(result['sessions']) < 2):
-        print "Number of sessions {0} less than 2".format(len(result['sessions']))
         exit_code = "2"
-    output_text = "Number of sessions: {0}".format(len(result['sessions']))
+    output_text = "Number of sessions: {0} {1}".format(len(result['sessions']), "less than 2" if len(result['sessions']) < 2 else "")
 
 passive_check_result = [dict(hostname='monitoring-station',service_description='AirVPN Status', return_code=exit_code,plugin_output=output_text)]
 
