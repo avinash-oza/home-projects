f9343e7369fba43daede8a63c5e8f01c2d4e79b4
Avi Oza <avioza@gmail.com>
2018-01-04 13:20:50 -0500

    Add script to pull from darksky and store

---
+++ pi/store_temperature/get_current_outdoor_temperature.py
@@ -0,0 +1,37 @@
+import pika
+import requests
+import configparser
+import datetime
+import json
+
+# Gets data from the darksky api
+
+config = configparser.ConfigParser()
+config.read('store_temperature.config')
+
+
+credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
+parameters = pika.URLParameters('amqp://rabbitmq:rabbitmq@172.16.1.10:5672')
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+channel.exchange_declare(exchange='temperatures', exchange_type='fanout')
+channel.queue_declare(queue='temperatures_mysql')
+channel.queue_declare(queue='temperatures_elasticsearch')
+channel.queue_bind(exchange='temperatures', queue='temperatures_mysql')
+channel.queue_bind(exchange='temperatures', queue='temperatures_elasticsearch')
+
+dark_sky_api_key = config.get('DARKSKY', 'api_key')
+location_coordinates = config.get('DARKSKY', 'location_coordinates')
+url = "https://api.darksky.net/forecast/{0}/{1}?exclude=minutely,hourly,daily,alerts,flags".format(dark_sky_api_key, location_coordinates)
+
+data = requests.get(url).json()
+
+data_dict = dict(sensor_name="OUTDOOR",
+                 sensor_value=data['currently']['temperature'],
+                 reading_time=datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S%p'),
+                 current_temperature=True)
+print(data_dict)
+
+channel.basic_publish(exchange='temperatures', routing_key='', body=json.dumps(data_dict))
