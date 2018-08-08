fbd978080c800922bc6f36da7f949294f0db9a5b
Avi Oza <avioza@gmail.com>
2018-08-07 21:49:06 -0400

    Remove out ansible as it is in a diff repo

--- ansible-testing/roles/install-telegram-bot/templates/start_telegram_bot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source {{ telegram_bot_virtual_env_location }}/bin/activate
-cd {{ telegram_checkout_location }}
-nohup python start_telegram_bot.py >> /dev/null 2>&1&
--- ansible-testing/sample_commands.sh
+++
@@ -1,2 +0,0 @@
-# Runs book to install vim for the vagrant box. Does not need sudo and uses ssh config file
-ansible-playbook install-and-configure-vim.yaml -i default, 
