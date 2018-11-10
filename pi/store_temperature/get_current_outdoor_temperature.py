6c20b763d8481db80799892d930d2f393a839c99
Avi Oza <avioza@gmail.com>
2018-11-10 18:51:22 -0500

    switch outdoor temperature to use queue as well

--- pi/store_temperature/get_current_outdoor_temperature.py
+++ pi/store_temperature/get_current_outdoor_temperature.py
@@ -1,8 +1,8 @@
-import pika
 import requests
 import configparser
 import datetime
 import json
+import boto3
 
 # Gets data from the darksky api
 
@@ -10,26 +10,20 @@ config = configparser.ConfigParser()
 config.read('store_temperature.config')
 
 
-credentials = pika.PlainCredentials('rabbitmq', 'rabbitmq')
-parameters = pika.URLParameters('amqp://rabbitmq:rabbitmq@172.16.1.10:5672')
-
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
-
-channel.exchange_declare(exchange='temperatures', exchange_type='fanout')
-channel.queue_declare(queue='temperatures_mysql')
-channel.queue_declare(queue='temperatures_elasticsearch')
-channel.queue_bind(exchange='temperatures', queue='temperatures_mysql')
-channel.queue_bind(exchange='temperatures', queue='temperatures_elasticsearch')
-
 dark_sky_api_key = config.get('DARKSKY', 'api_key')
 location_coordinates = config.get('DARKSKY', 'location_coordinates')
 url = "https://api.darksky.net/forecast/{0}/{1}?exclude=minutely,hourly,daily,alerts,flags".format(dark_sky_api_key, location_coordinates)
 
-data = requests.get(url).json()
-
-data_dict = dict(sensor_name="OUTDOOR",
-                 raw_value=data['currently']['temperature'],
-                 status_time=datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p'),
-                 current_temperature=True)
-channel.basic_publish(exchange='temperatures', routing_key='', body=json.dumps([data_dict]))
+try:
+    data = requests.get(url, timeout=5).json()
+except Exception as e:
+    print("Exception occured getting data from dark sky")
+else:
+    # construct value for queue
+    data_dict = dict(sensor_name="OUTDOOR",
+                     raw_value=data['currently']['temperature'],
+                     status_time=datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p'),
+                     current_temperature=True)
+    sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName='temperatures')
+    queue.send_message(MessageBody=json.dumps([data_dict]))
