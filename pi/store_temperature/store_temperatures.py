b4c2a822b8f23c9b93cac1c61f1713d3f823a72c
Avi Oza <avioza@gmail.com>
2018-11-11 11:46:47 -0500

    Update script to keep polling until no messages

--- pi/store_temperature/store_temperatures.py
+++ pi/store_temperature/store_temperatures.py
@@ -20,21 +20,30 @@ db_connection = mysql.connector.connect(user=db_user_name,password=db_password,h
 sqs = boto3.resource('sqs')
 queue = sqs.get_queue_by_name(QueueName='temperatures')
 
-for message in queue.receive_messages(MaxNumberOfMessages=10,WaitTimeSeconds=20):
-# retrieve SQS messages
+# keep trying till the queue is empty
+while True:
+    messages = queue.receive_messages(MaxNumberOfMessages=10,WaitTimeSeconds=20)
+    # retrieve SQS messages
+    if not messages:
+        print("nothing to write")
+        break
+    # list to delete all messages at once
+    delete_message_ids = []
+    for message in messages:
+        try:
+            entries = json.loads(message.body)
+        except:
+            logger.exception("Exception while parsing queue data:", body)
+        else:
+            print("Got messages")
+            cursor = db_connection.cursor()
+            logger.info(entries)
+            for one_entry in entries:
+                one_entry['formatted_time'] = datetime.datetime.strptime(one_entry['status_time'], "%Y-%m-%d %I:%M:%S %p")
 
-    try:
-        entries = json.loads(message.body)
-    except:
-        logger.exception("Exception while parsing queue data:", body)
-    else:
-        cursor = db_connection.cursor()
-        logger.info(entries)
-        for one_entry in entries:
-            one_entry['formatted_time'] = datetime.datetime.strptime(one_entry['status_time'], "%Y-%m-%d %I:%M:%S %p")
-
-            query = "INSERT INTO temperature.temperatures(sensor_name, sensor_value, reading_time) VALUES (%(sensor_name)s, %(raw_value)s, %(formatted_time)s)"
-            cursor.execute(query, one_entry)
-            db_connection.commit()
-            logger.info("Inserted entry")
-        message.delete()
+                query = "INSERT INTO temperature.temperatures(sensor_name, sensor_value, reading_time) VALUES (%(sensor_name)s, %(raw_value)s, %(formatted_time)s)"
+                cursor.execute(query, one_entry)
+                db_connection.commit()
+                logger.info("Inserted entry")
+            delete_message_ids.append({'Id': message.message_id, 'ReceiptHandle': message.receipt_handle})
+    queue.delete_messages(Entries=delete_message_ids)
