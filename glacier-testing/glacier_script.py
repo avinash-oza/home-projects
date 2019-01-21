57768bdc8acaaa22cf2d8761ce5c2706ee2927cb
Avi Oza <avioza@gmail.com>
2019-01-20 22:09:58 -0500

    fix typo and generate multipart upload id before upload

--- glacier-testing/glacier_script.py
+++ glacier-testing/glacier_script.py
@@ -10,7 +10,7 @@ import tarfile
 import boto3
 import gnupg
 
-# from glacier_upload import upload
+from glacier_upload.upload import GlacierUploadException, upload
 
 upload_id_regex = re.compile("Upload id: (.*)")
 
@@ -69,7 +69,7 @@ def get_or_create_multipart_upload(file_path, vault_name, part_size_bytes):
     pending_uploads = glacier.list_multipart_uploads(vaultName=vault_name)
     if pending_uploads['UploadsList']:
         # check to see if any are the current file we are upload
-        for one_upload in pending_uploads['UploadList']:
+        for one_upload in pending_uploads['UploadsList']:
             if one_upload['ArchiveDescription'] == file_path:
                 upload_id = one_upload['MultipartUploadId']
                 logger.info("Found pending upload id {}".format(upload_id))
@@ -121,82 +121,13 @@ def upload_file_to_glacier(file_path, vault_name, part_size=1024, threads=1):
     for t in range(5):
         logger.info("Starting glacier upload script for {}".format(file_path))
         try:
-            #TODO: fix the call to this script
-            run_output = subprocess.check_call("python glacier-upload/src/glacier_upload/upload.py -v {} -u {}".format(vault_name, upload_id), shell=True)
-        except subprocess.CalledProcessError as e:
-            logger.error("Retrying upload for {}".format(file_path))
+            result = upload(vault_name, file_name=[file_path], region=None, arc_desc=file_path, part_size=part_size, num_threads=threads, upload_id=upload_id)
+        except GlacierUploadException as e:
+            # set the upload id to use for resuming
+            logger.error("Retrying upload for {} with upload_id {}".format(file_path, upload_id))
         else:
-            logger.info("Successfully uploaded {}".format(file_path))
-            break
-
-    #TODO: figure out how to get the upload id here
-
-# def main(tar_dest_dir, base_dir, force_recreate):
-#     tar_dest_dir = os.path.join(tar_dest_dir, datetime.date.today().strftime('%Y-%m-%d'))
-#     logger.info("Create directory for run: {}".format(tar_dest_dir))
-#
-#     try:
-#         os.mkdir(tar_dest_dir)
-#     except FileExistsError:
-#         pass
-#
-#     # Init GPG class
-#     gpg = gnupg.GPG()
-#     key_to_use = gpg.list_keys()[0] # Assumption is the proper key is the only one here
-#     fingerprint = key_to_use['fingerprint']
-#     logger.info("Fingerprint of key is {} and uid is {}".format(fingerprint, key_to_use['uids']))
-#     part_size = 1024 # In mb
-#     threads = 1
-#
-#     with open('file_list.csv', 'r') as f:
-#         csv_reader = csv.DictReader(f)
-#         for row in csv_reader:
-#             if row['archive_always'].capitalize() == 'True' or force_recreate:
-#                 input_path = os.path.join(base_dir, row['directory_path'])
-#                 # file name based on dir name
-#                 output_file = '.'.join([os.path.basename(input_path).replace(' ', '_'), 'tar.gz'])
-#                 dest_tar_file = os.path.join(tar_dest_dir, output_file)
-#
-#                 encrypted_output = '.'.join([output_file, 'gpg'])
-#                 dest_gpg_encrypted_output = os.path.join(tar_dest_dir, encrypted_output)
-#
-#                 if not os.path.exists(dest_tar_file):
-#                     logger.info("Start tarring path: {}. Output path: {}".format(input_path, dest_tar_file))
-#
-#                     with tarfile.open(dest_tar_file, 'w:gz') as tar:
-#                         tar.add(input_path)
-#                     logger.info("Finished path: {}. Output path: {}".format(input_path, dest_tar_file))
-#
-#                 if not os.path.exists(dest_gpg_encrypted_output):
-#                     logger.info("Start GPG encrypting path: {} Output path: {}".format(dest_tar_file, dest_gpg_encrypted_output))
-#                     with open(dest_tar_file, 'rb') as tar_file:
-#                         ret = gpg.encrypt_file(tar_file, output=dest_gpg_encrypted_output, armor=False, recipients=fingerprint)
-#                     logger.info("{} {} {}".format(ret.ok, ret.status, ret.stderr))
-#                     logger.info("Finished GPG encrypting path: {}  Output path: {}".format(dest_tar_file, dest_gpg_encrypted_output))
-#
-#                 logger.info("Starting upload of {} to vault {}".format(dest_gpg_encrypted_output, row['vault_name']))
-#                 try:
-#                     # TODO: Inentional exception
-#                     raise subprocess.CalledProcessError(2, 'abcd', "Upload id: ")
-# #                   first_run_output = subprocess.check_call("python glacier-upload/src/glacier_upload/upload.py -v {} -f {} -d {} -p {} -t {}".format(row['vault_name'], dest_gpg_encrypted_output, str(dest_gpg_encrypted_output), part_size, threads), shell=True)
-#                 except subprocess.CalledProcessError as e:
-#                     upload_id = upload_id_regex.search(e.output).group(1)
-#                     logger.info("Connection dropped, retrying the upload with upload id {}".format(upload_id))
-#                     for t in range(5):
-# #                       sleep_time = random.randint(60,360)
-#                         sleep_time = 10
-#                         logger.info("************Trying {} time after {} sleep************".format(t, sleep_time))
-#                         time.sleep(sleep_time)
-#                         try:
-#                             output = subprocess.check_call("python glacier-upload/src/glacier_upload/upload.py -v {} -f {} -d {} -p {} -u {} -t {}".format(row['vault_name'], dest_gpg_encrypted_output, str(dest_gpg_encrypted_output), part_size, upload_id, threads), shell=True)
-#                         except subprocess.CalledProcessError as e:
-#                             pass # Retry from here
-#                         else:
-#                             break # finished uploaded file
-#
-#
-#                 logger.info("Finished upload of {} to vault {}".format(dest_gpg_encrypted_output, row['vault_name']))
-
+            logger.info("Successfully uploaded {} and archive_id is {} and location {}".format(file_path, result['archiveId'], result['location']))
+            return result
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(message)s')
