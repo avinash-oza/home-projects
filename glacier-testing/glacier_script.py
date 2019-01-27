715ed6519a712e67191123294c16f25dac42bde3
Avi Oza <avioza@gmail.com>
2019-01-26 22:29:25 -0500

    upload directory listings to S3 and other cleanup

--- glacier-testing/glacier_script.py
+++ glacier-testing/glacier_script.py
@@ -7,6 +7,7 @@ import logging
 import math
 import os
 import tarfile
+import subprocess
 
 import boto3
 import gnupg
@@ -40,7 +41,10 @@ class GlacierUploader:
 
     def upload_csv_to_s3(self, bucket_name, file_name, file_obj):
         file_obj.seek(io.SEEK_SET) # reset to beginning
-        encoded_data = io.BytesIO(file_obj.read().encode())
+        encoded_data = file_obj
+        if isinstance(file_obj, io.StringIO):
+            # convert StringIO to BytesIO for upload
+            encoded_data = io.BytesIO(file_obj.read().encode())
         self.s3.upload_fileobj(encoded_data, bucket_name, file_name)
         logger.info("Successfully wrote {} to bucket {}".format(file_name, bucket_name))
 
@@ -60,24 +64,21 @@ class GlacierUploader:
         archive_file_obj.write(temp_io.getvalue())
         logger.info("Wrote {} to archive log StringIO".format(kwargs))
 
-    def write_directory_list_to_file(self, file_path, temp_dir):
-        #TODO: update this to s3 as well
-        """writes the listing of the directory to a file"""
-        listing_file_name = '.'.join(['LISTING', os.path.basename(file_path).replace(' ', '_'),'gz'])
-        listing_file_path = os.path.join(temp_dir, listing_file_name)
-        logger.info("Creating file containing the list of files {}".format(listing_file_path))
-        with gzip.open(listing_file_path, 'wt') as f:
+    def write_directory_list_to_file(self, file_path):
+        """writes the directory listing out to a buffer for upload to S3"""
+        listing_file_name = os.path.join('listings', '.'.join([os.path.basename(file_path).replace(' ', '_'),'gz']))
+        listing_file_obj = io.BytesIO()
 
-            for dirname, dirnames, filenames in os.walk(file_path):
-                # print path to all subdirectories first.
-                for subdirname in dirnames:
-                    f.write(os.path.join(dirname, subdirname) +'\n')
+        logger.info("Creating file containing the list of files {}".format(listing_file_name))
 
-                # print path to all filenames.
-                for filename in filenames:
-                    f.write(os.path.join(dirname, filename) + '\n')
+        with gzip.GzipFile(filename=listing_file_name, mode='w', fileobj=listing_file_obj) as gzipped_listing:
+            # with gzip.open(listing_file_path, 'wt') as f:
+            result = subprocess.run('du -ah {}'.format(file_path), shell=True, capture_output=True)
+            gzipped_listing.write(result.stdout)
 
-        logger.info("Done creating file listing {}".format(listing_file_path))
+        logger.info("Done creating file {}".format(listing_file_name))
+        self.upload_csv_to_s3(self._bucket_name, listing_file_name, listing_file_obj)
+        return listing_file_obj
 
     def encrypt_and_compress_path(self, file_path, temp_dir):
         tar_dest_dir = os.path.join(temp_dir, datetime.date.today().strftime('%Y-%m-%d'))
@@ -244,7 +245,7 @@ class GlacierUploader:
             logger.info("Calling encrypt and compress with {} and vault {}".format(file_path, vault_name))
 
             gpg_file_path, gpg_file_name = self.encrypt_and_compress_path(file_path, temp_dir)
-            self.write_directory_list_to_file(file_path, temp_dir)
+            self.write_directory_list_to_file(file_path)
             result = self.upload_file_to_glacier(gpg_file_path, vault_name)
 
             self.write_line_to_archive(archive_file_obj, file_path=file_path, type=dir_type,
@@ -260,10 +261,8 @@ if __name__ == '__main__':
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.DEBUG)
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', type=str, help='Dir to tar')
-    parser.add_argument('--temp-dir', type=str, help='scratch space')
-
-    parser.add_argument('--input-file-path', type=str, help='File containing directory list')
+    parser.add_argument('--temp-dir', type=str, required=True, help='dir to use for scratch space')
+    parser.add_argument('--input-file-path', type=str, required=True, help='File containing directory list')
     args = parser.parse_args()
 
     g = GlacierUploader()
