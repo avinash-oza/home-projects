7ad01d696d96616d1a9c882fd2d0ed4000eb4fbd
Avi Oza <avioza@gmail.com>
2019-05-11 23:35:55 -0400

    Refactor to lambda format and use env vars

--- pi/store_temperature/darksky_retrieve/lambda_function.py
+++ pi/store_temperature/darksky_retrieve/lambda_function.py
@@ -1,29 +1,31 @@
 import requests
-import configparser
 import datetime
 import json
 import boto3
+import os
 
 # Gets data from the darksky api
+dark_sky_api_key = os.environ['DARKSKY_API_KEY']
+location_coordinates = os.environ['DARKSKY_API_COORDINATES']
 
-config = configparser.ConfigParser()
-config.read('store_temperature.config')
+sqs = boto3.resource('sqs')
+queue = sqs.get_queue_by_name(QueueName='temperatures')
 
 
-dark_sky_api_key = config.get('DARKSKY', 'api_key')
-location_coordinates = config.get('DARKSKY', 'location_coordinates')
-url = "https://api.darksky.net/forecast/{0}/{1}?exclude=minutely,hourly,daily,alerts,flags".format(dark_sky_api_key, location_coordinates)
+def lambda_handler(event, context):
+    url = "https://api.darksky.net/forecast/{0}/{1}?exclude=minutely,hourly,daily,alerts,flags".format(dark_sky_api_key, location_coordinates)
 
-try:
-    data = requests.get(url, timeout=5).json()
-except Exception as e:
-    print("Exception occured getting data from dark sky")
-else:
-    # construct value for queue
-    data_dict = dict(sensor_name="OUTDOOR",
-                     raw_value=data['currently']['temperature'],
-                     status_time=datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p'),
-                     current_temperature=True)
-    sqs = boto3.resource('sqs')
-    queue = sqs.get_queue_by_name(QueueName='temperatures')
-    queue.send_message(MessageBody=json.dumps([data_dict]))
+    try:
+        data = requests.get(url, timeout=3).json()
+    except Exception as e:
+        print("Exception occured getting data from dark sky")
+    else:
+        # construct value for queue
+        data_dict = dict(sensor_name="OUTDOOR",
+                         raw_value=data['currently']['temperature'],
+                         status_time=datetime.datetime.now().strftime('%Y-%m-%d %I:%M:%S %p'),
+                         current_temperature=True)
+        queue.send_message(MessageBody=json.dumps([data_dict]))
+        print("Sent message to queue")
+
+#ambda_handler(None, None)
