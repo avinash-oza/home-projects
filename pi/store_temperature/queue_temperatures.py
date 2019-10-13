9062832e2acffa833bc9a92e73e3e8260095288d
Avi Oza <avioza@gmail.com>
2019-10-13 17:52:38 -0400

    refactor script to allow it to run on the host itself

--- pi/store_temperature/queue_temperatures.py
+++ pi/store_temperature/queue_temperatures.py
@@ -1,17 +1,25 @@
+import argparse
+
 import boto3
 import requests
 
 
-urls_to_get = ['http://172.16.2.100:25002/temperature/all', 'http://172.16.2.102:25002/temperature/all']
+def main(endpoint_url):
+    sqs = boto3.resource('sqs')
 
-sqs = boto3.resource('sqs')
+    queue = sqs.get_queue_by_name(QueueName='temperatures')
 
-queue = sqs.get_queue_by_name(QueueName='temperatures')
-
-for url in urls_to_get:
     try:
-        body = requests.get(url, timeout=2)
+        body = requests.get(endpoint_url, timeout=2)
     except Exception as e:
-        print("Exception when trying to pull url {}".format(url))
+        print("Exception when trying to pull url {}".format(endpoint_url))
     else:
         queue.send_message(MessageBody=body.text)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--endpoint', default="http://localhost:25002/temperature/all")
+    args = parser.parse_args()
+
+    main(args.endpoint)
