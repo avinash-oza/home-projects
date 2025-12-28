b62a99adf1785b8c45a47338f638100e90d5a830
avi <avioza@gmail.com>
2025-12-28 17:51:06 -0500

    apply formatting on file and remove requirements.txt

--- other_scripts/google_calendar_events/add_gcal_events.py
+++ other_scripts/google_calendar_events/add_gcal_events.py
@@ -6,9 +6,9 @@ import pandas as pd
 from gcsa.event import Event
 from gcsa.google_calendar import GoogleCalendar
 
-logging.basicConfig(level=logging.INFO,
-                    format="[%(asctime)s] %(levelname)s %(message)s"
-                    )
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s] %(levelname)s %(message)s"
+)
 
 logger = logging.getLogger(__name__)
 
@@ -16,8 +16,12 @@ logger = logging.getLogger(__name__)
 def read_events_file(file_path):
     logger.info(f"Reading events from file_path={file_path}")
     events_df = pd.read_csv(file_path)
-    events_df['event_date_start'] = pd.to_datetime(events_df['event_date_start']).dt.tz_localize('America/New_York')
-    events_df['event_date_end'] = pd.to_datetime(events_df['event_date_end']).dt.tz_localize('America/New_York')
+    events_df["event_date_start"] = pd.to_datetime(
+        events_df["event_date_start"]
+    ).dt.tz_localize("America/New_York")
+    events_df["event_date_end"] = pd.to_datetime(
+        events_df["event_date_end"]
+    ).dt.tz_localize("America/New_York")
 
     logger.info(f"Read {len(events_df)} events from file_path={file_path}")
 
@@ -26,30 +30,39 @@ def read_events_file(file_path):
 
 def add_events(events_df, calendar, dry_run):
     for idx, row in events_df.iterrows():
-        event_name = row['event_name']
-        start_dt = row['event_date_start']
-        end_dt = row['event_date_end']
-        location = row['location']
+        event_name = row["event_name"]
+        start_dt = row["event_date_start"]
+        end_dt = row["event_date_end"]
+        location = row["location"]
 
-        logger.info(f"Adding event_name={event_name} with start={start_dt},end={end_dt},location={location}")
+        logger.info(
+            f"Adding event_name={event_name} with start={start_dt},end={end_dt},location={location}"
+        )
 
         if not dry_run:
-            e = Event(
-                event_name,
-                start=start_dt,
-                end=end_dt,
-                location=location
-            )
+            e = Event(event_name, start=start_dt, end=end_dt, location=location)
             calendar.add_event(e)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Adds events from a file to calendar')
-    parser.add_argument('--calendar-id', type=str, help='Google calendar id')
-    parser.add_argument('--wipe-calendar', action='store_true', help='Wipe the calendar before adding events')
-    parser.add_argument('--dry-run', action='store_true', help="Print out what is being done but don't do it")
-    parser.add_argument('--events-file-path', type=str, required=True,
-                        help='CSV file containing events to be added')
+    parser = argparse.ArgumentParser(description="Adds events from a file to calendar")
+    parser.add_argument("--calendar-id", type=str, help="Google calendar id")
+    parser.add_argument(
+        "--wipe-calendar",
+        action="store_true",
+        help="Wipe the calendar before adding events",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help="Print out what is being done but don't do it",
+    )
+    parser.add_argument(
+        "--events-file-path",
+        type=str,
+        required=True,
+        help="CSV file containing events to be added",
+    )
 
     args = parser.parse_args()
     calendar_id = args.calendar_id
@@ -67,8 +80,8 @@ def main():
 
     if not dry_run and wipe_calendar:
         logger.info(f"Wiping calendar before adding new events to it")
-        s = df_events['event_date_start'].min()
-        e = df_events['event_date_start'].max() + timedelta(hours=1)
+        s = df_events["event_date_start"].min()
+        e = df_events["event_date_start"].max() + timedelta(hours=1)
         for e in cal.get_events(time_min=s, time_max=e):
             logger.info(f"Deleting event={e.summary}")
             cal.delete_event(e)
@@ -76,5 +89,5 @@ def main():
     add_events(df_events, cal, dry_run)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
